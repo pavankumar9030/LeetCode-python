672dfa1edf52306610877c21409b1772fcc25d97
Koppula Pavan kumar <9924051010@klu.ac.in>
2026-02-20 10:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (17.74%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def makeLargestSpecial(self, s):
+        count = 0
+        start = 0
+        parts = []
+        
+        for i in range(len(s)):
+            if s[i] == '1':
+                count += 1
+            else:
+                count -= 1
+            
+            if count == 0:
+                inner = self.makeLargestSpecial(s[start+1:i])
+                parts.append("1" + inner + "0")
+                start = i + 1
+        
+        parts.sort(reverse=True)
+        return "".join(parts)
