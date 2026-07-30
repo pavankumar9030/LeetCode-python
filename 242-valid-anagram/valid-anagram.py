21d71a621b6a37006920f121948e36cb36c9ed27
Koppula Pavan kumar <9924051010@klu.ac.in>
2026-07-30 20:48:45 +0530

    Time: 23 ms (50.12%) | Memory: 13.4 MB (28.47%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if sorted(s)==sorted(t):
+            return True
+        else:
+            return False
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
