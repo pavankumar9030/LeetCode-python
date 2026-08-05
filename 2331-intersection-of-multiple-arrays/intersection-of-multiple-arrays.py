b64f423fff125453de1fee9fe3071ff0dbde5d0c
Koppula Pavan kumar <9924051010@klu.ac.in>
2026-08-05 20:46:46 +0530

    Time: 11 ms (15.75%) | Memory: 12.6 MB (15.38%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,24 +1,17 @@
-class Solution:
+class Solution(object):
     def intersection(self, nums):
-
-        count = {}
-
-        for arr in nums:
-
-            for num in arr:
-
-                if num in count:
-                    count[num] += 1
-                else:
-                    count[num] = 1
-
-        result = []
-
-        for key in count:
-
-            if count[key] == len(nums):
-                result.append(key)
-
-        result.sort()
-
-        return result
+        result=[]
+        for i in nums[0]:
+            found=True
+            for j in range(1,len(nums)):
+                if i not in nums[j]:
+                    found=False
+                    break
+            if found==True:
+                result.append(i)
+        return sorted(result)
+        """
+        :type nums: List[List[int]]
+        :rtype: List[int]
+        """
+        
