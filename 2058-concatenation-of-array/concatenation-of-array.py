ec56f2d70dfeb9cb064757d011a9532f0ab795bf
Koppula Pavan kumar <9924051010@klu.ac.in>
2026-08-10 20:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (34.84%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        result=list(nums*2)
+        return result
+        
+        
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
