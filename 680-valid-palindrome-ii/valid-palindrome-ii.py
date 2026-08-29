2f57ea56f5850d54318fa71974094148ed6b40dc
Koppula Pavan kumar <9924051010@klu.ac.in>
2026-08-29 20:55:25 +0530

    Time: 43 ms (89.25%) | Memory: 12.7 MB (43.71%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s):
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+
+            if s[left] != s[right]:
+
+                return (s[left:right] == s[left:right][::-1] or
+                        s[left+1:right+1] == s[left+1:right+1][::-1])
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
